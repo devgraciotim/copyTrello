6ea6b6e1c90e8ff2d399b0db0b1805d10666662c
coradoo <conrado.matheus@protonmail.com>
2023-10-10 20:28:29 -0300

    iusfkdhjskdals

--- mathew/src/js/script.js
+++ mathew/src/js/script.js
@@ -1,4 +1,20 @@
-function addTask() {
-    const taskNameInput = document.getElementById("taskNameInput").value
-    console.log(taskNameInput)
+function addTask(columnId) {
+    const nameInput = document.getElementById(`${columnId}-nameInput`).value
+    const descInput = document.getElementById(`${columnId}-descInput`).value
+    const taskCreated = createTask(nameInput, descInput)
+    document.getElementById(`${columnId}-taskContainer`).append(taskCreated)
+}
+
+function createTask(nameInput, descInput) {
+
+    const taskContainer = document.createElement("div")
+    taskContainer.className = "taskContainer"
+    const taskName = document.createElement("h3")
+    taskName.innerText = nameInput
+    const taskDesc = document.createElement("p")
+    taskDesc.innerText = descInput
+
+    taskContainer.append(taskName, taskDesc)
+
+    return taskContainer
 }
